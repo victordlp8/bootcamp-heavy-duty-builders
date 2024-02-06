276dbdaf583c1e4111d0aa24e27b7bd90098e7a4
victordlp8 <victordlp8@gmail.com>
2024-02-06 22:34:04 +0100

    Fixed lit process bug
    #1 Arreglado

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,6 +1,6 @@
-import { Buffer } from 'buffer';
-import * as process from 'process';
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { Buffer } from "buffer";
 
 (window as any).global = window;
 (window as any).global.Buffer = Buffer;
-(window as any).process = process;
+(window as any).process = { env: { }};  
